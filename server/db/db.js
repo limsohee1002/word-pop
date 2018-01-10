842eba2b2b971718cde1c102be3f826d8b5e6492
sohee Lim <limsohee1002@gmail.com>
2018-01-10 15:41:38 -0500

    db connect

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,8 @@
 //require sequelize and connect to the aws server, using MySQL
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(config.database, config.user, config.password, {
-  host: config.host,
+require('dotenv').config()
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   logging: false, //stops sequelize from outputting sql to the terminal 
   pool: {
